df58f19fda99dd4184aa457046c9c80b418b8454
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2023-08-24 03:26:16 +0530

    commodity ui

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -4,6 +4,7 @@ import 'package:frontend/auth/roles.dart';
 import 'package:frontend/controllers/polygon_controller.dart';
 import 'package:frontend/controllers/user_controller.dart';
 import 'package:frontend/views/classifier/disease.dart';
+import 'package:frontend/views/commodity/commodityview.dart';
 import 'package:frontend/views/home/homepage.dart';
 import 'package:frontend/views/polygon/polygon.dart';
 
@@ -13,7 +14,14 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'controllers/nav_controller.dart';
 
-class Navigation extends StatelessWidget {
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
   final controller = Get.put(NavigationController());
   final polygon = Get.put(PolygonController());
   final user = Get.put(UserController());
@@ -33,9 +41,7 @@ class Navigation extends StatelessWidget {
                 },
               ),
               DiseaseView(),
-              Container(
-                color: Colors.blue,
-              ),
+              CommodityView(),
               ProfileScreen(),
             ],
           ),
@@ -64,7 +70,7 @@ class Navigation extends StatelessWidget {
                 duration: const Duration(milliseconds: 500),
                 tabBackgroundColor: Colors.green,
                 color: Colors.black38,
-                tabs: [
+                tabs: const [
                   GButton(
                     icon: LineIcons.home,
                     text: 'Home',
@@ -74,8 +80,8 @@ class Navigation extends StatelessWidget {
                     text: 'Detector',
                   ),
                   GButton(
-                    icon: LineIcons.bell,
-                    text: 'Reminders',
+                    icon: LineIcons.lineChart,
+                    text: 'Market',
                   ),
                   GButton(
                     icon: LineIcons.user,
@@ -91,3 +97,82 @@ class Navigation extends StatelessWidget {
   }
 }
 
+
+// class Navigation extends StatelessWidget {
+//   final controller = Get.put(NavigationController());
+//   final polygon = Get.put(PolygonController());
+//   final user = Get.put(UserController());
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetBuilder<NavigationController>(builder: (controller) {
+//       return Scaffold(
+//         backgroundColor: Colors.white,
+//         body: SafeArea(
+//           child: IndexedStack(
+//             index: controller.tabIndex,
+//             children: [
+//               FutureBuilder(
+//                 future: polygon.getData(user.user.value.polygonId.toString()),
+//                 builder: (context, snapshot) {
+//                   return Homepage();
+//                 },
+//               ),
+//               DiseaseView(),
+//               Container(
+//                 color: Colors.blue,
+//               ),
+//               ProfileScreen(),
+//             ],
+//           ),
+//         ),
+//         bottomNavigationBar: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.white,
+//             boxShadow: [
+//               BoxShadow(
+//                 blurRadius: 20,
+//                 color: Colors.black.withOpacity(.1),
+//               ),
+//             ],
+//           ),
+//           child: SafeArea(
+//             child: Padding(
+//               padding:
+//               const EdgeInsets.symmetric(horizontal: 22.0, vertical: 12),
+//               child: GNav(
+//                 onTabChange: controller.changeTabIndex,
+//                 gap: 8,
+//                 activeColor: Colors.white,
+//                 iconSize: 24,
+//                 padding:
+//                 const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+//                 duration: const Duration(milliseconds: 500),
+//                 tabBackgroundColor: Colors.green,
+//                 color: Colors.black38,
+//                 tabs: const [
+//                   GButton(
+//                     icon: LineIcons.home,
+//                     text: 'Home',
+//                   ),
+//                   GButton(
+//                     icon: LineIcons.pollH,
+//                     text: 'Detector',
+//                   ),
+//                   GButton(
+//                     icon: LineIcons.lineChart,
+//                     text: 'Market',
+//                   ),
+//                   GButton(
+//                     icon: LineIcons.user,
+//                     text: 'Profile',
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       );
+//     });
+//   }
+// }
+//
