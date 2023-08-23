2efd0b54a37ed5d147572613aa834ebc0c2e7334
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2023-08-23 12:38:45 +0530

    login page ui

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -10,6 +10,70 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size;
+    return Container(
+      // color: Pallete.backgroundColor,
+      child: SafeArea(
+        child: Scaffold(
+          // backgroundColor: Pallete.whiteColor,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/login_page_gif.gif'),
+              Container(),
+              const Spacer(),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding:  EdgeInsets.only(left: size.width * 0.1),
+                  child: SizedBox(
+                    height: size.height * 0.2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome',
+                          // style: largeHeading,
+                        ),
+                        Text(
+                          'You have been missed!',
+                          // style: largeHeading2,
+                        ),
+                        Text(
+                          'Login to explore options',
+                          // style: subHeading,
+                        ),
+                        const Spacer(),
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Pallete.secondaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          fixedSize: Size(size.width * 0.7, size.height * 0.07),
+                        ),
+                        onPressed: signInWithGoogle,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset('assets/images/google_icon.png', height: 20, width: 20,	),
+
+                            const Text('Login with Google', style: TextStyle(fontWeight: FontWeight.bold),),
+
+                          ],
+                        ),
+                      ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
