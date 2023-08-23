1ac5a8b73fb0458ae53bce309ba3f10f332f451c
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2023-08-23 14:47:15 +0530

    Choose roles UI

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: AppTheme.theme,
-      home: LoginScreen(),
+      home: Navigation(), //LoginScreen()
     );
   }
 }
