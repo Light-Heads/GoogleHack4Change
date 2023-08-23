7bafc5aede50bcdbccc38bcdc4560a9ba4270371
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2023-08-23 12:26:45 +0530

    auth implementation setup google sign in

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthController extends GetxController {
+  var auth = FirebaseAuth.instance;
+ Future<void> signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication; 
+
+      if (googleAuth?.accessToken != null && googleAuth?.idToken != null) {
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken,
+          idToken: googleAuth?.idToken,
+        );
+        UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        // userCredential.additionalUserInfo!.isNewUser
+        //     ? Navigator.push(context, MaterialPageRoute(builder: (context) {
+        //         // return RegisterPage();
+        //       }))
+        //     : Navigator.push(context, MaterialPageRoute(builder: (context) {
+        //         // return HomePage();
+        //       }));
+      }
+    }catch(e){
+      Get.snackbar('Error', e.toString());
+    }
+  }
+}
