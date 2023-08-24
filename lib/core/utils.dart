8ac00830c52986e5afa239f900a407fde0e0e8ee
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2023-08-24 07:21:42 +0530

    Ploygon Ui DOne

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -15,7 +15,61 @@ void showSnackBar(BuildContext context, String content) {
 }
 
 
-void calculateHealth()
-{
 
+
+String calculateHealth(double NDVI)
+{
+  if(NDVI <0.0 || NDVI >=  0.2)
+    {
+      return "Low";
+    }
+  else if(NDVI <0.2 || NDVI >=  0.4)
+  {
+    return "Medium";
+  }
+  else if(NDVI <0.4 || NDVI >=  0.6)
+  {
+    return "High";
+  }
+  else
+    {
+      return "High";
+    }
+}
+
+String calculateWaterStress(double NDWI, double  DSWI)
+{
+  double res = (NDWI + DSWI) / 2;
+  if(res < 0.2)
+  {
+    return "Good";
+  }
+  else if(res <= 0.2 || res >= 0.5 )
+  {
+    return "Medium";
+  }
+  else if(res >= 0.5)
+  {
+    return "High";
+  }
+  else
+  {
+    return "High";
+  }
+}
+
+String calculateNitrogen(double NRI)
+{
+  if(NRI <= 0.2 )
+    {
+      return "Low";
+    }
+  else if(NRI >0.2)
+    {
+      return "High";
+    }
+  else
+    {
+      return "Decent";
+    }
 }
